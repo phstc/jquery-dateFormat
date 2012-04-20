97901f537986e6a90c05d0319a029b611d1669b7
Washington Botelho <wbotelhos@gmail.com>
2012-04-20 16:11:17 -0300

    Added semicolon at the begin to avoid scripts conflict on minified.

--- jquery.dateFormat-1.0.js
+++ jquery.dateFormat-1.0.js
@@ -1,4 +1,4 @@
-(function (jQuery) {
+;(function (jQuery) {
 		
 		var daysInWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 		var shortMonthsInYear = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
