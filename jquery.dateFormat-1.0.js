54949727e61f12ffb9ad460eed024adb1acec598
Michael Fasani <michaelfasani@gmail.com>
2013-07-31 21:56:34 +0100

    The word ago was missing after 31 days

--- jquery.dateFormat-1.0.js
+++ jquery.dateFormat-1.0.js
@@ -363,7 +363,7 @@
 					return;
 
 				if (day_diff >= 31)
-					return "more than 31 days";
+					return "more than 31 days ago";
 
 				return day_diff == 0 && (diff < 60 && "just now" || diff < 120 && "1 minute ago" || diff < 3600 && Math.floor(diff / 60) + " minutes ago" || diff < 7200 && "1 hour ago" || diff < 86400 && Math.floor(diff / 3600) + " hours ago") || day_diff == 1 && "Yesterday" || day_diff < 7 && day_diff + " days ago" || day_diff < 31 && Math.ceil(day_diff / 7) + " weeks ago";
 			},
