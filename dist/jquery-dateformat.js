3fc08ede4cd7e3af7104bb91615805c8a16780f7
Pablo Cantero <pablo@pablocantero.com>
2018-03-11 19:07:00 -0400

    MOAR updates

--- dist/jquery-dateformat.js
+++ dist/jquery-dateformat.js
@@ -488,3 +488,8 @@ var DateFormat = {};
     };
   }());
 }(DateFormat));
+;// require dateFormat.js
+// please check `dist/jquery.dateFormat.js` for a complete version
+(function($) {
+  $.format = DateFormat.format;
+}(jQuery));
